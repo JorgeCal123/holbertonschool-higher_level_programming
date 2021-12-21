780134684fda1ed47fc741e65cdf9599cc518a53
JorgeCal123/boom.git <git@github.com>
2021-12-21 07:49:20 -0800

    punto 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if (number > 0):
+    last_digit = number % 10
+else:
+    last_digit = -number % 10
+
+if (last_digit == 0):
+    compl = "and is 0"
+elif (last_digit <= 5):
+    compl = "and is less than 6 and not 0"
+else:
+    compl = "and is greater than 5"
+
+msm = "Last digit of " + str(number) + " is " + str(last_digit) + " " + compl
+print(msm)
