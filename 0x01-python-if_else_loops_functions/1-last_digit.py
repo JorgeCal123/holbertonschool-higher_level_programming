2193bc70fbe9ed84b545c0c76bee015de1eb56a2
JorgeCal123/boom.git <git@github.com>
2021-12-21 09:17:55 -0800

    punto 1 corr 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 if (number > 0):
     last_digit = number % 10
 else:
-    last_digit = -number % 10
+    last_digit = -(-number % 10)
 
 if (last_digit == 0):
     compl = "and is 0"
