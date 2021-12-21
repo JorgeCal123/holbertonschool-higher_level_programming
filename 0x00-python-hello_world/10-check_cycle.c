12c109e7b94656a8f0f900dd19955a5aa248c3a7
JorgeCal123/boom.git <git@github.com>
2021-12-20 20:17:07 -0800

    punto 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * check_cycle - check if a loop exists within a linked list
+ * @list: linked list type listint_t to check
+ * Return: 1 if exist loop and if not exist loop
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *copy = list;
+
+	copy = copy->next;
+	while (copy->next)
+	{
+		copy = copy->next;
+		if (list->n == copy->n)
+			return (1);
+
+	}
+	return (0);
+}
