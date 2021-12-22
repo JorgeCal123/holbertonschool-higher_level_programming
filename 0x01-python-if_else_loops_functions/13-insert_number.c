91191cd902739a5d3b429e4d8cb63b63fde83889
JorgeCal123/boom.git <git@github.com>
2021-12-21 19:16:07 -0800

    punto 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -16,6 +16,8 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *temp_Next;
 
 	new_n = malloc(sizeof(listint_t));
+	if (!new)
+	return (NULL);
 	while(original)
 	{
 		if(original->n < number)
