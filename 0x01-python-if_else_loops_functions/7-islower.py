b5ab80a75c545fc74d72918c0d3d6e3238d3d5a7
JorgeCal123/boom.git <git@github.com>
2021-12-21 16:15:55 -0800

    punto 7 corr 5

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def islower(c):
-    if ord(c) >= ord('a') and ord(c) <= ord('z'):
-        return (True)
-    else:
-        return (False)
+    for i in range(ord("a"), ord("z")):
+        if(ord(c) == i):
+            return (True)
+    return (False)
