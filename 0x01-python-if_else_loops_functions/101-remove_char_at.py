1d66e99aaf90b717b1be1edcf039435ccc40d279
JorgeCal123/boom.git <git@github.com>
2021-12-21 20:19:40 -0800

    punto 101 corr 5

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,5 +2,5 @@
 def remove_char_at(str, n):
     b = n + 1
     if n >= 0:
-    msm = str[:n] + str[b:]
-    return ("{}".format(msm))
+        str = str[:n] + str[b:]
+    return ("{}".format(str))
