278a99a89567201c8fdd5b259a76241192ab5e22
JorgeCal123/boom.git <git@github.com>
2021-12-21 19:14:15 -0800

    punto 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,31 @@
+#include <stddef.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_node - inserts a number into a sorted singly linked list.
+ * @head: is a linked list type listint_t
+ * @number: number into a sorted
+ * Return: linked list updated
+ */
+listint_t *insert_node(listint_t **head, int number)
+{	
+	listint_t *original = *head;
+	listint_t *new_n; 
+	listint_t *before;
+	listint_t *temp_Next;
+
+	new_n = malloc(sizeof(listint_t));
+	while(original)
+	{
+		if(original->n < number)
+			before = original;
+	original = original->next; 
+	}
+	temp_Next = before->next;
+	new_n->n = number;
+	new_n->next = temp_Next;
+	before->next = new_n;
+
+	return(original);
+}
