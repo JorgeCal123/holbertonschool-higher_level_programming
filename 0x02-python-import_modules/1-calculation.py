ea88518046dc9e224061786f9fed1ce6c3cfd007
JorgeCal123/boom.git <git@github.com>
2021-12-23 14:38:37 -0800

    punto 1 corr 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-import calculator_1 as f
-print("10 + 5 = {:d}\n10 - 5 = {:d}\n10 * 5 = {:d}\n10 / 5 = {:d}".format(f.add(10, 5), f.sub(10, 5), f.mul(10, 5), f.div(10, 5)))
+if __name__ == "__main__":
+
+    import calculator_1 as f
+    print("10 + 5 = {:d}\n10 - 5 = {:d}\n10 * 5 = {:d}\n10 / 5 = {:d}".format(f.add(10, 5), f.sub(10, 5), f.mul(10, 5), f.div(10, 5)))
