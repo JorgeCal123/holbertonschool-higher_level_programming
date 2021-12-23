50763e5ad2817d57caa3c23482bc57d4a4139250
JorgeCal123/boom.git <git@github.com>
2021-12-23 13:08:55 -0800

    punto 0 corr 8

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import add_0 as f
-    print("{} + {} = {}".format("1", "2", f.add(1, 2)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, f.add(1, 2)))
