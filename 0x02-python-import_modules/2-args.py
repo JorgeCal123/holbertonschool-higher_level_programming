76068d2a78ddbaa15c7fa5665ea579c717cb8403
JorgeCal123/boom.git <git@github.com>
2021-12-23 17:26:44 -0800

    punto 2 corr 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,9 @@
 if __name__ == "__main__":
     import sys
     lenght = len(sys.argv)
-    print("{:d} arguments:".format(lenght -1))
+    if (len == 1):
+        print("{:d} argument:".format(lenght - 1))
+    else:
+        print("{:d} arguments:".format(lenght - 1))
     for i in range(1, lenght):
-        print(str(i) + ": "+ sys.argv[i])
+        print(str(i) + ": " + sys.argv[i])
