b2a219b4dce8d110364b34e3f0496e18c57eae47
JorgeCal123/boom.git <git@github.com>
2021-12-23 18:00:29 -0800

    punto 3 corr 1

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-import sys
-lenght = len(sys.argv)
+if __name__ == "__main__":
+    import sys
+    lenght = len(sys.argv)
+    suma = 0
+    for i in range(1, lenght):
+        suma+=int(sys.argv[i])
+    print(suma)
