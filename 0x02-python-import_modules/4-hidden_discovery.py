cd37aecf6225a1df6cefcace79ce047d78e3b736
JorgeCal123/boom.git <git@github.com>
2021-12-23 18:32:16 -0800

    punto 4 corr 3

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     arr = dir(hidden_4)
     for i in range(0, len(arr)):
-        if file[i][0:2] != "__":
+        if arr[i][0:2] != "__":
             print(arr[i])
