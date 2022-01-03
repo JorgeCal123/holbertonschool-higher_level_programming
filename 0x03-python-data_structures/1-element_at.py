167047543890fa1f710ca5bfc75b2478e2ad6e7c
JorgeCal123/boom.git <git@github.com>
2022-01-03 11:56:16 -0800

    punto 1 corr 3

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if (idx < 0):
-        return None
-    elif (len(my_list) < idx):
-        return None
+        return
+    elif ((len(my_list) - 1) < idx):
+        return
     else:
         return(my_list[idx])
