566295012a3d3db655474003a10b8ca65e879fd1
JorgeCal123/boom.git <git@github.com>
2022-01-03 14:06:27 -0800

    punto 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        if (i != 0):
+            print()
+        for j in range (len(matrix[0])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if(j != (len(matrix[0]) - 1)):
+                print("", end=" ")
+    print()
