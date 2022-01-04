8fcbe7f88d2621eb3cb7dfd1f08b09a42628eeb0
JorgeCal123/boom.git <git@github.com>
2022-01-03 18:53:51 -0800

    punto 7 corr 4

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-
-    l1 = list(tuple_a)
-    l2 = list(tuple_b)
-    while (len(l1) != 2 or len(l2) != 2):
-        if (len(l2) < 2):
-            l2.append(0)
-        elif(len(l1) < 2):
-            l1.append(0)
-
-    new_tuple = (l1[0] + l2[0], l1[1] + l2[1])
-    return new_tuple
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
