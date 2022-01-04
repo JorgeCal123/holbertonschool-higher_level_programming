5f4f6bc63e6c6138d34c426c49b3b194c4cd7677
JorgeCal123/boom.git <git@github.com>
2022-01-03 17:24:40 -0800

    punto 8 corr 2

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if (sentence is None):
-        return (0, None)
+    if (sentence == ""):
+        return None
     return (len(sentence), sentence[0])
