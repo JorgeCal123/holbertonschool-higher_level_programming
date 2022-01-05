aa4f262c992e20c12bf5965d1c54ae87d2cb12b3
JorgeCal123/boom.git <git@github.com>
2022-01-05 13:57:26 -0800

    punto 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = [replace 
+            if my_list[i] == search
+            else my_list[i]
+            for i in range(len(my_list))]
+    return new_list
