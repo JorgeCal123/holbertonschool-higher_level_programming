5d214eeb28bd2fe390a313b5ff5957ee0fc25f33
JorgeCal123/boom.git <git@github.com>
2022-01-05 19:47:59 -0800

    punto 10 corr 3

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,8 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary)
+    score = list(a_dictionary.keys())[0]
+    for key in a_dictionary:
+        if a_dictionary[key] > a_dictionary[score]:
+            score = key
+    return score
