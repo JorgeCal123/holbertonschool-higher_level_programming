16dc5a998ea2bb84e629a1c0988ad4ff08313393
JorgeCal123/boom.git <git@github.com>
2022-01-05 21:20:06 -0800

    punto 12 corr 1

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,17 @@
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman = {
+        "I": 1,
+        "IV": 4,
+        "V": 5,
+        "IX": 9,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
     number = []
     for i in roman_string:
         number.append(roman.get(i))
@@ -12,10 +22,8 @@ def roman_to_int(roman_string):
     while j < len(number) - 1:
 
         if(number[j] < number[j + 1]):
-        #    print(number[j], number[j + 1], total)
             total = number[j + 1] - total
         elif(number[j] >= number[j + 1]):
-         #   print(number[j], number[j + 1], total)
             total = number[j + 1] + total
         j += 1
 
