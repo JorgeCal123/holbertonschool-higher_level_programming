086b317f5a5a11575a256975037103749e578604
JorgeCal123/boom.git <git@github.com>
2022-01-17 10:32:48 -0800

    punto 2 err 5

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,8 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     j = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
-            j += 1
+            if isinstance(my_list[i], int):
+                print("{}".format(my_list[i]), end="")
+                j += 1
         except (ValueError, TypeError):
             continue
     print()
