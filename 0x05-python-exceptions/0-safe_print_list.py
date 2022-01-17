f8fba40c06f603a2a87ff251dbceb369a8609e7d
JorgeCal123/boom.git <git@github.com>
2022-01-17 09:23:35 -0800

    punto 0 corr 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             j += 1
         print()
-    except IndexError:    
+    except IndexError:
         print()
 
     return (j)
