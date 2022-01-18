84a7d66e3d668042b31845e91d5b45d2e9267c6f
JorgeCal123/boom.git <git@github.com>
2022-01-18 14:26:19 -0800

    punto 2 corr 4

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,16 +6,8 @@ class Square:
 
     """Method constructor"""
     def __init__(self, size=0):
-        self.size = size
-
-    @property
-    def size(self):
-        return self._size
-
-    @size.setter
-    def size(self, size):
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self._size = size
+        self.__size = size
