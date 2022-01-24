452b4bd47042211caf713a6c3aaca3f070a1cd82
JorgeCal123/boom.git <git@github.com>
2022-01-24 15:47:42 -0800

    punto 7 corr 2

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -5,10 +5,9 @@
 
 class Rectangle:
     """Rectangle class defined by width and height."""
-
-
     number_of_instances = 0
     print_symbol = "#"
+
 
     def __init__(self, width=0, height=0):
         self.height = height
