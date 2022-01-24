29525428974c215c8a8fcd1b80a62ad69b1f3ef4
JorgeCal123/boom.git <git@github.com>
2022-01-24 15:57:09 -0800

    punto 9 corr 1

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -4,10 +4,9 @@
 
 
 class Rectangle:
+    """Rectangle class defined by width and height."""
     number_of_instances = 0
     print_symbol = "#"
-
-    """Rectangle class defined by width and height."""
 
     def __init__(self, width=0, height=0):
         self.height = height
@@ -45,10 +44,10 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if not isinstance (rect_1, Rectangle):
-            raise TypeError ("rect_1 must be an instance of Rectangle")
-        if not isinstance (rect_2, Rectangle):
-            raise TypeError ("rect_2 must be an instance of Rectangle")
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
         else:
