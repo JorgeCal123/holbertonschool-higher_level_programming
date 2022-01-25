70daa15100292f91ef5c6da2e9653a5687822fba
JorgeCal123/boom.git <git@github.com>
2022-01-24 16:07:47 -0800

    punto 9 corr 2

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -8,6 +8,12 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
+
+    @classmethod
+    def square(cls, size=0):
+        """returns a new Rectangle"""
+        return cls(size, size)
+
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
@@ -52,11 +58,6 @@ class Rectangle:
             return rect_1
         else:
             return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        """returns a new Rectangle"""
-        return cls(size, size)
 
     @property
     def width(self):
