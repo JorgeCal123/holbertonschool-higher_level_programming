6478a194c710da4100c589a1f1c18436343f80c7
JorgeCal123/boom.git <git@github.com>
2022-01-31 15:12:52 -0800

    punto 3 corr 1

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """is_kind_of_class"""
 
+
 def is_kind_of_class(obj, a_class):
     """object is an instance of, or if the object is an instance of a class"""
     if isinstance(obj, a_class):
