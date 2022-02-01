b17dd98e3ca9dcff334a26706f2cef571c7c84f2
JorgeCal123/boom.git <git@github.com>
2022-02-01 12:35:41 -0800

    punto 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""read_file"""
+
+
+def read_file(filename=""):
+    """"function that reads a text file (UTF8) and prints it to stdout:"""
+    with open(filename) as f:
+        read_data = f.read()
+        print(read_data, end="")
