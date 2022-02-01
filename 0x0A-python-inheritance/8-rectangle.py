fdffcf03c56de97bcd35268fd0a7ce2c447383e4
JorgeCal123/boom.git <git@github.com>
2022-01-31 19:34:52 -0800

    punto 8 corr 2

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,5 +8,7 @@ class Rectangle(BaseGeometry):
     """Rectangle data inherits from BaseGeometry"""
 
     def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
