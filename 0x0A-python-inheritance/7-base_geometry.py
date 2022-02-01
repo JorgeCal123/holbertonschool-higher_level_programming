d35ceb9faef69095758c7aaf34f3e44786af3b20
JorgeCal123/boom.git <git@github.com>
2022-01-31 19:02:07 -0800

    punto 7 corr 5

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""BaseGeometry"""
+""" BaseGeometry """
 
 
 class BaseGeometry:
-    """empty class"""
+    """
+    Adding integers validator method
+    """
     def area(self):
+        """
+        Nothing happens
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+        Validate integer
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        
