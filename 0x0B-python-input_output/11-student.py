15b74b4c8a47c73abe3845cba0396b478795d260
JorgeCal123/boom.git <git@github.com>
2022-02-01 18:41:09 -0800

    punto 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """ Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """method constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        student = {}
+        a = vars(self)
+        if attrs is None:
+            return a
+        for key, val in a.items():
+            if key in attrs:
+                student[key] = val
+        return student
+
+    def reload_from_json(self, json):
+        """meth string"""
+        for k, v in json.items():
+            setattr(self, k, v)
