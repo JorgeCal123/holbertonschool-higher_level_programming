3f510f25f3cad7a901c8680525a8a5306fa2ddbc
JorgeCal123/boom.git <git@github.com>
2022-02-01 18:20:39 -0800

    punto 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """ Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """method constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
+        a = vars(self)
+        return a
