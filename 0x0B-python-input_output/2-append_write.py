d613ac540f5de8db819423fe1a017ea83574c348
JorgeCal123/boom.git <git@github.com>
2022-02-01 16:00:41 -0800

    punto 2 corr 1

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,7 @@
 
 
 def append_write(filename="", text=""):
-    """"""
+    """function that appends a string at the end of a text file"""
     with open(filename, 'a+',) as f:
         f.write(text)
     return len(text)
