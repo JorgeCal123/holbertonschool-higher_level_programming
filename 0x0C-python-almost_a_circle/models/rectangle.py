c8f158dec517a673dc73852a7f500f881b077d3b
jorgecal123 <git@github.com>
2022-02-06 20:36:09 -0800

    punto 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,6 +26,9 @@ class Rectangle(Base):
                 print("#", end="")
             print()
 
+    def __str__(self):
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height))
+
     @property
     def width(self):
         """get width"""
