18883e02b3a6e6ed79a6c9189d4ed3a1d9713dfd
jorgecal123 <git@github.com>
2022-02-07 12:05:04 -0800

    punto 1 corr 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """Class Base"""
 
+
 class Base:
     """Class father"""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id != None:
+        """method constructor"""
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
