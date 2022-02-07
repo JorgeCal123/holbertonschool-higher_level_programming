17bed50b297032be93b5a459bd561bfd08f67476
jorgecal123 <git@github.com>
2022-02-07 11:58:46 -0800

    punto 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class square"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square that inherits from Rectangle"""
+
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y)
+
+    def __str__(self):
+        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width))
