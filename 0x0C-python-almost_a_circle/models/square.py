3b15e23cddd603eb2feeb2364806e17128cb838d
jorgecal123 <git@github.com>
2022-02-07 12:37:14 -0800

    punto 10 corr 2

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,7 +7,7 @@ class Square(Rectangle):
     """Square that inherits from Rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         return ("[Square] ({}) {}/{} - {}".format(
