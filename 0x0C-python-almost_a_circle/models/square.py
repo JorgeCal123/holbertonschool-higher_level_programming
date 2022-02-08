ffb9be9a4d660fec3b08eedacb20d949d2ccdcb6
JorgeCal123 <git@github.com>
2022-02-07 16:14:28 -0800

    punto 11 corr 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,18 +7,21 @@ class Square(Rectangle):
     """Square that inherits from Rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """method constructor"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """method str"""
         return ("[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width))
 
     @property
     def size(self):
+        """get size"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """set size"""
         self.width = value
-        self.heigh= value
-
+        self.heigh = value
