0b8a8882b79b963449dde762bd8b5927d9ec4824
JorgeCal123 <git@github.com>
2022-02-07 16:23:41 -0800

    test punto 10

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -13,3 +13,11 @@ class TestSquare(unittest.TestCase):
         self.assertIsInstance(square1, Base)
         self.assertIsInstance(square1, Square)
         self.assertEqual(square1.id, 1)
+
+    def test_set_size(self):
+        square2 = Square(15)
+        square2.size = 10
+        self.assertEqual(square2.size, 10)
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            square2.size = "10"
+
