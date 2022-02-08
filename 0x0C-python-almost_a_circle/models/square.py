80ce7bec486fc166eef0ee761c80466c1f5e1a05
jorgecal123 <git@github.com>
2022-02-07 16:41:56 -0800

    punto 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,6 +15,27 @@ class Square(Rectangle):
         return ("[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width))
 
+    def update(self, *args, **kwargs):
+        if args:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.size = args[1]
+                if i == 2:
+                    self.x = args[2]
+                if i == 3:
+                    self.y = args[3]
+        else:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
+
     @property
     def size(self):
         """get size"""
