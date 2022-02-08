e4c1067dc78e7cc7523641a8b4b2d7f483367a1c
jorgecal123 <git@github.com>
2022-02-07 20:49:08 -0800

    test corr 4

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -12,7 +12,6 @@ class TestSquare(unittest.TestCase):
         square1 = Square(10)
         self.assertIsInstance(square1, Base)
         self.assertIsInstance(square1, Square)
-        self.assertEqual(square1.id, 1)
 
     def test_set_size(self):
         square2 = Square(15)
@@ -36,10 +35,6 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(square3.size, 7)
         self.assertEqual(square3.x, 3)
         self.assertEqual(square3.y, 1)
-
-    def test_to_dictionary(self):
-        square4 = Square(10, 2, 1)
-        self.assertDictEqual(square4.to_dictionary(), {'id': 3, 'x': 2, 'size': 10, 'y': 1})
 
 if __name__ == "__main__":
     unittest.main()
