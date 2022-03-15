1825c9ca53cca0d1292f7c47aa4566baf31e4571
JorgeCal123/boom.git <git@github.com>
2022-03-15 12:47:23 -0700

    task 1 corr 6

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,2 @@
---Write a script that creates the database hbtn_0c_0
---in your MySQL server.
-
-DROP DATABASE IF EXISTS hbtn_0c_0;
-CREATE DATABASE hbtn_0c_0;
+-- Write a script that creates the database hbtn_0c_0 in your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
