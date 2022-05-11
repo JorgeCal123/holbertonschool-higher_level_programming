36a9b76055b4b51216f37b783fbb7effcba71f32
JorgeCal123/boom.git <git@github.com>
2022-05-11 15:44:37 -0700

    task 15

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -8,22 +8,20 @@ to the database hbtn_0e_100_usa"""
 import sys
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
+    sesionCRUD = Session(engine)
 
-    session = Session(engine)
-    new_state = State(name='California')
-    new_city = City(name='San Francisco')
-    new_state.cities.append(new_city)
+    myState = State(name='California')
+    myCity = City(name='San Francisco')
+    myState.cities.append(myCity)
+    sesionCRUD.add(myState)
 
-    session.add(new_state)
-    session.commit()
-    session.close()
+    sesionCRUD.commit()
+    sesionCRUD.close()
