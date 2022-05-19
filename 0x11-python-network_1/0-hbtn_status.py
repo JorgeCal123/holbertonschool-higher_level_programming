1078a5f5c42e0b5027c70de22d234c5908380694
JorgeCal123/boom.git <git@github.com>
2022-05-19 16:16:43 -0700

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,4 +6,6 @@ import urllib.request as request
 if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status/') as response:
         html = response.read()
-        print('Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'.format(type(html), html, html.decode('utf-8')))
+        print('Body response:\n\t\
+        - type: {}\n\t- content: {}\n\t\
+        - utf8 content: {}'.format(type(html), html, html.decode('utf-8')))
